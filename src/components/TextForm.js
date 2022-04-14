c9086b9e26d50ddbb874e7bb050a21fd838d0bb6
Muhammad Noman Naeem <59162949+Noman-8560@users.noreply.github.com>
2022-04-15 02:29:34 +0500

    Find text and word length

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,12 +13,20 @@ export default function TextForm(props) {
 
   const [text ,setText] = useState('Enter a Text');
   return (
-<div>
+    <>
+<div className='container'>
 <h2>{props.heading}</h2>    
 <div className="mb-3">
 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 </div>
 <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button> 
 </div>
+<div className='container my-3'>
+  <h1>Your Text Summary</h1>
+  <p>{text.split(" ").length} words, {text.length} characters</p>
+  
+</div>
+</>
+
   )
 }
