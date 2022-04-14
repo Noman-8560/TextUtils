8864a11138e535a82d613513324870114494ee2a
Muhammad Noman Naeem <59162949+Noman-8560@users.noreply.github.com>
2022-04-15 03:00:23 +0500

    Find text and word length

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,12 +6,16 @@ export default function TextForm(props) {
     const newText = text.toUpperCase();
     setText(newText);
   }
+  const handleLoClick = () => {
+    const newText = text.toLowerCase();
+    setText(newText);
+  }
   const handleOnChange = (event) =>{
      setText(event.target.value);
   }
 
 
-  const [text ,setText] = useState('Enter a Text');
+  const [text ,setText] = useState('');
   return (
     <>
 <div className='container'>
@@ -19,12 +23,15 @@ export default function TextForm(props) {
 <div className="mb-3">
 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 </div>
-<button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button> 
+<button className='btn btn-primary mx-1' onClick={handleUpClick}>Convert to Uppercase</button>
+<button className='btn btn-primary mx-1' onClick={handleLoClick}>Convert to Lowercase</button>  
 </div>
 <div className='container my-3'>
-  <h1>Your Text Summary</h1>
+  <h2>Your Text Summary</h2>
   <p>{text.split(" ").length} words and {text.length} characters</p>
-  <p>{0.08 * text.split(" ").length} Minutes Read </p>
+  <p>{0.08 * text.split(" ").length} Minutes Read</p>
+  <h2>Preview</h2>
+  <p>{text}</p>
   
 </div>
 </>
