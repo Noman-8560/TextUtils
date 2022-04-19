3200a98ea6d5abcf6525836290bebbe0f3b3995b
Muhammad Noman Naeem <59162949+Noman-8560@users.noreply.github.com>
2022-04-19 23:02:44 +0500

    Enable Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-// import logo from './logo.svg';
 import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [mode , setMode] = useState('light');
+  const toggleMode = () =>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor= '#212529';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor= 'white';
+    }
+  }
+
   return (
-  
     <>
-     <Navbar title="Textutils" title1="Home" title2="About" />
+     <Navbar title="Textutils" title1="Home" title2="About" mode={mode} toggleMode={toggleMode} />
      <div className='container my-3'>
      <TextForm heading='Enter a Text to Analyze Below' />
      {/* <About/> */}
      </div>
     </>
-
-
   );
 }
 
