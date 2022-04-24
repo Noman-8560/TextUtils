ae2943a51059099e056a9383660e4547f0d99dca
Muhammad Noman Naeem <59162949+Noman-8560@users.noreply.github.com>
2022-04-24 16:38:20 +0500

    Adding Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
+import { BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 
 
 function App() {
@@ -37,12 +38,21 @@ function App() {
 
   return (
     <>
+    <Router>
      <Navbar title="Textutils" title1="Home" title2="About" mode={mode} toggleMode={toggleMode} />
      <Alert  alert={alert} />
      <div className='container my-3'>
-     <TextForm showAlert={showAlert} heading='Enter a Text to Analyze Below' mode={mode} />
-     {/* <About/> */}
+        <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              
+              <Route path="/">
+              <TextForm showAlert={showAlert} heading='Enter a Text to Analyze Below' mode={mode} />
+              </Route>
+        </Switch>
      </div>
+     </Router>
     </>
   );
 }
